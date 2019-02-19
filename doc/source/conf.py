707e60b3a21770e38388574003d4288bb704109c
Sergey Avseyev <sergey.avseyev@gmail.com>
2019-02-19 09:49:57 +0300

    fix imgpath instead of pngmath in sphinx-doc
        WARNING: sphinx.ext.pngmath has been deprecated. Please use sphinx.ext.imgmath instead.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,7 @@ import os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.imgmath', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
 
 # Breathe setup, for integrating doxygen content
 extensions.append('breathe')

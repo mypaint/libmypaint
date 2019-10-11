5707894daea69ffb52acccd36b524d4a95706466
Sabri Ünal <libreajans@gmail.com>
2019-10-11 23:14:34 +0300

    free the self to prevent leak

--- mypaint-fixed-tiled-surface.c
+++ mypaint-fixed-tiled-surface.c
@@ -120,6 +120,7 @@ mypaint_fixed_tiled_surface_new(int width, int height)
     uint16_t * buffer = (uint16_t *)malloc(buffer_size);
     if (!buffer) {
         fprintf(stderr, "CRITICAL: unable to allocate enough memory: %zu bytes", buffer_size);
+        free(self);
         return NULL;
     }
     memset(buffer, 255, buffer_size);

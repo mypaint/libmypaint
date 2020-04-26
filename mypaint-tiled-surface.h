329dcd8bf78d5f9c6d761200dc0f6a7fa89407a0
Jesper Lloyd <jpl.lloyd@gmail.com>
2020-04-26 22:13:21 +0200

    Document mypaint-tiled-surface.h

--- mypaint-tiled-surface.h
+++ mypaint-tiled-surface.h
@@ -26,7 +26,7 @@ typedef struct {
     int ty;
     /*! Whether the tile data should be considered read-only */
     gboolean readonly;
-    /*! Pointer to the tile buffer (set by requestee)*/
+    /*! Pointer to the tile buffer, set by receiver of the request */
     guint16 *buffer;
     /*! Additional data to be used by surface implementations __(unused)__.*/
     gpointer context; /* Only to be used by the surface implementations. */
@@ -45,7 +45,20 @@ void
 mypaint_tile_request_init(MyPaintTileRequest *data, int level,
                           int tx, int ty, gboolean readonly);
 
+/*!
+ * Function for beginning a tile request from the surface backend
+ *
+ * @memberof MyPaintTiledSurface
+ * @sa MyPaintTileRequest, MyPaintTileRequestEndFunction
+ */
 typedef void (*MyPaintTileRequestStartFunction) (MyPaintTiledSurface *self, MyPaintTileRequest *request);
+
+/*!
+ * Function for ending a tile request from the surface backend
+ *
+ * @memberof MyPaintTiledSurface
+ * @sa MyPaintTileRequest, MyPaintTileRequestStartFunction
+ */
 typedef void (*MyPaintTileRequestEndFunction) (MyPaintTiledSurface *self, MyPaintTileRequest *request);
 
 /*!
@@ -54,19 +67,33 @@ typedef void (*MyPaintTileRequestEndFunction) (MyPaintTiledSurface *self, MyPain
  * Interface and convenience class for implementing a MyPaintSurface backed by
  * a tile store.
  *
- * The size of the surface is infinite, and consumers need just implement two
- * vfuncs.
+ * The size of the surface is infinite, and consumers only need to provide
+ * implementations for #tile_request_start and #tile_request_end
+ *
+ * @sa MyPaintTiledSurface2
  */
 struct MyPaintTiledSurface {
+    /*! Surface interface */
     MyPaintSurface parent;
-    /* private: */
+    /* "private": */
+    /*! See #MyPaintTileRequestStartFunction */
     MyPaintTileRequestStartFunction tile_request_start;
+    /*! See #MyPaintTileRequestEndFunction */
     MyPaintTileRequestEndFunction tile_request_end;
+    /*! Whether vertical-line symmetry is enabled or not */
     gboolean surface_do_symmetry;
+    /*! The x-coordinate of the vertical symmetry line */
     float surface_center_x;
+    /*! Per-tile queue of pending dab operations */
     struct OperationQueue *operation_queue;
+    /*!
+     * Invalidation rectangle recording areas changed between the calls to
+     * #parent%'s MyPaintSurface::begin_atomic and MyPaintSurface::end_atomic
+     */
     MyPaintRectangle dirty_bbox;
+    /*! Whether tile requests shuold be considered thread-safe or not */
     gboolean threadsafe_tile_requests;
+    /*! The side length of the (square) tiles */
     int tile_size;
 };
 
@@ -100,7 +127,7 @@ mypaint_tiled_surface_destroy(MyPaintTiledSurface *self);
  * Set the symmetry state of the surface.
  *
  * When the symmetry is active, for each dab drawn with
- * @ref mypaint_surface_draw_dab, reflected horizontally across the
+ * ::mypaint_surface_draw_dab, reflected horizontally across the
  * vertical line defined by MyPaintTiledSurface.surface_center_x.
  *
  * @param active Whether symmetry should be used or not.
@@ -114,7 +141,7 @@ mypaint_tiled_surface_set_symmetry_state(MyPaintTiledSurface *self, gboolean act
 /*!
  * Get the average alpha value of pixels covered by a standard dab.
  *
- * Equivalent to @ref mypaint_surface_get_alpha
+ * Equivalent to ::mypaint_surface_get_alpha
  * (this function should probably not have been made public).
  *
  * @memberof MyPaintTiledSurface
@@ -147,7 +174,7 @@ void mypaint_tiled_surface_tile_request_end(MyPaintTiledSurface *self, MyPaintTi
  * Implementation of MyPaintSurface::begin_atomic
  * Note: Only intended to be used from MyPaintTiledSurface subclasses,
  * which should chain up to this if overriding MyPaintSurface::begin_atomic.
- * Application code should only use @ref mypaint_surface_begin_atomic
+ * Application code should only use #mypaint_surface_begin_atomic
  *
  * @memberof MyPaintTiledSurface
  */
@@ -157,7 +184,13 @@ void mypaint_tiled_surface_end_atomic(MyPaintTiledSurface *self, MyPaintRectangl
 
 /* -- Extended interface -- */
 
+/*! Functionally equivalent to #MyPaintTileRequestStartFunction
+ * @memberof MyPaintTiledSurface2
+ */
 typedef void (*MyPaintTileRequestStartFunction2) (MyPaintTiledSurface2 *self, MyPaintTileRequest *request);
+/*! Functionally equivalent to #MyPaintTileRequestEndFunction
+ * @memberof MyPaintTiledSurface2
+ */
 typedef void (*MyPaintTileRequestEndFunction2) (MyPaintTiledSurface2 *self, MyPaintTileRequest *request);
 
 /*!
@@ -167,17 +200,33 @@ typedef void (*MyPaintTileRequestEndFunction2) (MyPaintTiledSurface2 *self, MyPa
   * supports additional symmetry types, and the ability to adjust the symmetry
   * angle - it is otherwise identical to MyPaintTiledSurface.
   *
+  * @sa MyPaintTiledSurface
   */
 struct MyPaintTiledSurface2 {
+  /*! Parent interface */
   MyPaintSurface2 parent;
+  /*! See #MyPaintTileRequestStartFunction2 */
   MyPaintTileRequestStartFunction2 tile_request_start;
+  /*! See #MyPaintTileRequestEndFunction2 */
   MyPaintTileRequestEndFunction2 tile_request_end;
+  /*! Per-tile queue of pending dab operations */
   struct OperationQueue *operation_queue;
+  /*! Whether tile requests shuold be considered thread-safe or not */
   gboolean threadsafe_tile_requests;
   int tile_size;
+  /*! The symmetry data used
+   *
+   * See MyPaintSymmetryData for details.
+   */
   MyPaintSymmetryData symmetry_data;
+  /*! Length of #bboxes */
   int num_bboxes;
+  /*! The number of #bboxes that have been modified since they were last reset */
   int num_bboxes_dirtied;
+  /*! Pointer to an array of invalidation rectangles
+   *
+   * Records multiple invalidation rectangles when symmetry is enabled.
+   */
   MyPaintRectangle* bboxes;
 };
 
@@ -196,15 +245,48 @@ mypaint_tiled_surface2_init(
   MyPaintTileRequestEndFunction2 tile_request_end
   );
 
+/*!
+ * Prepare the surface for handling a set of dab operations.
+ *
+ * @memberof MyPaintTiledSurface2
+ */
 void mypaint_tiled_surface2_begin_atomic(MyPaintTiledSurface2 *self);
+
+/*!
+ * Finalize any pending dab operations and set the resulting invalidation rectangles.
+ *
+ * @memberof MyPaintTiledSurface2
+ */
 void mypaint_tiled_surface2_end_atomic(MyPaintTiledSurface2 *self, MyPaintRectangles *roi);
 
+/*!
+ * Finalize any pending dab operations and set the resulting invalidation rectangles.
+ *
+ * @memberof MyPaintTiledSurface2
+ */
 void mypaint_tiled_surface2_tile_request_start(MyPaintTiledSurface2 *self, MyPaintTileRequest *request);
+
+/*!
+ * Finalize any pending dab operations and set the resulting invalidation rectangles.
+ *
+ * @memberof MyPaintTiledSurface2
+ */
 void mypaint_tiled_surface2_tile_request_end(MyPaintTiledSurface2 *self, MyPaintTileRequest *request);
 
+/*!
+ * Deallocate all resources used by the surface struct, and the struct itself.
+ *
+ * @memberof MyPaintTiledSurface2
+ */
 void
 mypaint_tiled_surface2_destroy(MyPaintTiledSurface2 *self);
 
+/*!
+ * Set new #symmetry_data values and mark it for update
+ *
+ * @memberof MyPaintTiledSurface2
+ * @sa MyPaintSymmetryData, MyPaintSymmetryState
+ */
 void
 mypaint_tiled_surface2_set_symmetry_state(MyPaintTiledSurface2 *self, gboolean active,
                                          float center_x, float center_y,
